58e8ac77a3b40e85a52703d06a2ee59af944b8e2
sumanredd <gsumanthreddy15@gmail.com>
2025-09-27 20:20:11 +0530

    mobile responsivness

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import NavBar from './components/Navbar';
-import './App.css';
 import HeroSec from './components/HeroSec';
 import Features from './components/Features';
 import StudentReviews from './components/Testimonials';
 import Footer from './components/Footer';
+import { BeatLoader } from 'react-spinners';
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+  
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <NavBar/>
-      <HeroSec/>
-      <Features/>
-      <StudentReviews/>
-      <Footer/>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="spinner">
+  <BeatLoader color="#2563eb" size={15} margin={5} />
+</div>
+      ) : (
+        <div>
+          <NavBar />
+          <HeroSec />
+          <Features />
+          <StudentReviews />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,29 @@
+import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GiBookCover } from "react-icons/gi";
 import './index.css'
 
-const NavBar =()=>{
-    return(
-        <nav>
-           <h1 className="Logo">Navbar</h1>
-           <div style={{marginBottom:"8px" ,display:"flex",alignItems:"center"}}>
-           <ul className="NavUl">
-                <li className="li">Home</li>
-                <li className="li">Courses</li>
-                <li className="li">Mock Tests</li>
-                <li className="li">Contact</li>
-                <li className="li">About Us</li>
-           </ul>
-           <button className="loginBtn">Log in</button>
-           </div>
-        </nav>
-    )
-}
-export default NavBar
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="shadow-sm">
+      <Container>
+        <GiBookCover size={35}/>
+        <Navbar.Brand className="logoText" href="#">IELTS Institute</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="li" href="#">Home</Nav.Link>
+            <Nav.Link className="li" href="#">Courses</Nav.Link>
+            <Nav.Link className="li" href="#">Mock Tests</Nav.Link>
+            <Nav.Link className="li" href="#">Contact</Nav.Link>
+            <Nav.Link className="li" href="#">About Us</Nav.Link>
+          </Nav>
+          <Button className="loginBtn" variant="primary">Log in</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
